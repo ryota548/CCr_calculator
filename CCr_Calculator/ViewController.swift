6d03c0e3a4e3521adb2e27cde5d5f96c65be13b4
有村琢磨 <arimuratakuma@aritaku.local>
2015-10-31 16:31:02 +0900

    first commit

--- CCr_Calculator/ViewController.swift
+++ CCr_Calculator/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sexSegmented: UISegmentedControl!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var ccrTextField: UITextField!
+    
+    var age :Double = 0
+    var weight :Double = 0
+    var ccr :Double = 0
+    
+    var result :Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        resultLabel.text = String(0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func stringToDouble(){
+        age = NSString(string: ageTextField.text!).doubleValue 
+        weight = NSString(string: weightTextField.text!).doubleValue
+        ccr = NSString(string: ccrTextField.text!).doubleValue 
+    }
+    
+    @IBAction func calculate(sender: AnyObject) {
+       result = ((140 - age) * weight) / (72 * ccr)
+        result * 
+    
+    }
 
 }
 
